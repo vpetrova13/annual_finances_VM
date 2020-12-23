083b7b2293c9da95afa47dc183563b89aaac1848
vpetrova13 <vpetrova1364@gmail.com>
2020-12-23 20:19:20 +0000

    started analysis

--- cleanning_data_script/cleaning_finance_data.R
+++ cleanning_data_script/cleaning_finance_data.R
@@ -1,14 +1,16 @@
 ## Load libraries and read data
 library(tidyverse)
 library(janitor)
-raw_data <- read_csv("~/annual_finances/raw_data/MOROZV10660239-20201101.csv")
+library(here)
 
-## Remove unneded columns 
+raw_data <- read_csv(here("raw_data/MOROZV10660239-20201101.csv"))
+
+## Remove unneeded columns 
 finance_data <- raw_data %>% 
   clean_names() %>% 
   select(-account_number, -account_name, -x8)
 
-
+## Change abbreviations to full names
 finance_data <- finance_data %>% 
   mutate(type = str_replace_all(type, "D/\\D", replacement = "direct debit")) %>% 
   mutate(type = str_replace_all(type, "DPC", "digital banking payment")) %>% 
@@ -19,7 +21,7 @@ finance_data <- finance_data %>%
   mutate(type = str_replace_all(type, "C/\\L", "cash line")) %>% 
   mutate(type = str_replace_all(type, "ITL", "international transfer")) 
 
-
+## Optimise description for further easier analysis
 finance_data_clean <- finance_data %>% 
   mutate(date = str_remove(date, "^[0-9]{2}\\/")) %>% 
   mutate(description = str_to_lower(description)) %>% 
@@ -34,9 +36,7 @@ finance_data_clean <- finance_data %>%
   mutate(description = str_remove_all(description, "[:punct:]+")) 
 
 
-
-
-
+## Assign each payment to group
 finance_data_clean <- finance_data_clean %>% 
   mutate(categories = case_when(description == "personal  loan" ~ "car loan",
                                 description == "council"  ~ "council tax",
@@ -129,8 +129,8 @@ finance_data_clean <- finance_data_clean %>%
                                 description == "happy  beds  dewsbury  refund" ~ "bonus",
                                 description == "vitalij  petrov  debt  online  pymt" ~ "parents"))
 
-
-unique(finance_data_clean$categories)
+## Write csv file of clean data
+write_csv(finance_data_clean, "finance_clean_data.csv")
 
 
 
